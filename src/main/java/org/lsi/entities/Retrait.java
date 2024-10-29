6dfdeb8f3c4383b3706e53e46ebeefef236e1ac6
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-10-29 19:32:10 +0100

    First commit

---
+++ src/main/java/org/lsi/entities/Retrait.java
@@ -0,0 +1,7 @@
+package org.lsi.entities;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("R")
+public class Retrait extends Operation {
+}
---
+++ src/main/java/org/lsi/entities/Versment.java
@@ -0,0 +1,8 @@
+package org.lsi.entities;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("V")
+public class Versment extends Operation {
+
+}
